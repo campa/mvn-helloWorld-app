a09dc537220cb013204cf838bf0f9cc20f033a22
Stefano Emilio Campanini <stefano.campanini@gmail.com>
2016-07-08 15:06:58 +0200

    Initial commit

---
+++ src/main/java/net/tinvention/training/mvn/hw/app/App.java
@@ -0,0 +1,13 @@
+package net.tinvention.training.mvn.hw.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
